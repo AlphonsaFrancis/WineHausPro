ea742cfa8d964ac32801be30e1c540bbe63d75ff
AlphonsaFrancis <155885284+AlphonsaFrancis@users.noreply.github.com>
2024-08-10 01:09:03 +0530

    forget password api

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 const config = {
     registrationApi: process.env.REACT_APP_REGISTER_API ,
     loginApi: process.env.REACT_APP_LOGIN_API ,
+    passwordResetApi:process.env.REACT_APP_FORGET_PASSWORD_API
     // Add other configurations as needed
   };
    
--- src/pages/authentication/Login.js
+++ src/pages/authentication/Login.js
@@ -1,76 +1,133 @@
-import React, { useState } from 'react';
-import google from '../../assets/googleicon.png'  
-import config from '../../config/config';
-import axios from 'axios';
+import React, { useState } from "react";
+import google from "../../assets/googleicon.png";
+import config from "../../config/config";
+import axios from "axios";
 
-const Login = ({setIslogin}) => {
-
-  const [username,setUsername]  = useState('');
-  const [password, setPassword] = useState('');
-  const [error,setError] = useState();
-
-  const formSubmit = (e)=>{
-    e.preventDefault()
-    console.log({username, password})
-    if(username===""||password===""){
-      setError('All fields are required !')
+const Login = ({ setIslogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState();
+  const [isforgetpassword, setisforgetpassword] = useState(false);
+  const formSubmit = (e) => {
+    e.preventDefault();
+    console.log({ username, password });
+    if (username === "" || password === "") {
+      setError("All fields are required !");
+    } else {
+      setError("");
+      const formData = new FormData();
+      formData.append("email", username);
+      formData.append("password", password);
+      axios
+        .post(`${config.loginApi}`, formData)
+        .then((response) => {
+          console.log(response.data);
+          if (response.status === 200) {
+            console.log("Login Succesfull");
+            alert("Login Successfull");
+          }
+          // setIslogin(true)
+        })
+        .catch((error) => {
+          console.log(error);
+          setError(error.response.data.error);
+        });
     }
-    else{
-      setError('');
-      const formData=new FormData()
-      formData.append('email',username)
-      formData.append('password',password)
-      axios.post(`${config.loginApi}`,formData)
-      .then((response)=>{
-        console.log(response.data)
-        if (response.status===200){
-          console.log("Login Succesfull")
-          alert("Login Successfull")
-        }
-        // setIslogin(true)
-      }
-      )
-      .catch((error)=>{
-        console.log(error)
-        setError(error.response.data.error)
-      }
-      )
-    }
-  }
+  };
+
+  const resetPassword = () => {
+    console.log("reset password");
+    setisforgetpassword(true);
+  };
+  const requestresetpassword = () => {
+    const formData = new FormData();
+    formData.append("email", username);
+    axios
+      .post(`${config.passwordResetApi}`, formData)
+      .then((response) => {
+        console.log(response.data);
+        if (response.status === 200)
+          alert("Reset password link sent to your email");
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error.response.data.error);
+      });
+  };
   return (
-      <div className="right-panel">
-        <h1>WineHaus</h1>
-        <h2>Let Us Start..</h2>
+    <div className="right-panel">
+      <h1>WineHaus</h1>
+      <h2>Let Us Start..</h2>
+      {!isforgetpassword ? (
         <form onSubmit={formSubmit}>
-          <label htmlFor="username">User name or Email</label>
-          <input type="text" id="username" name="username" placeholder="Username" value={username} onChange={(e)=>setUsername(e.target.value)} />
-          
+          <label htmlFor="username">Email</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} />
-          
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
           <div className="forgot-password">
-            <a href="#">Forgot password?</a>
+            <a href="#" onClick={resetPassword}>
+              Forgot password?
+            </a>
           </div>
 
-          <p className='form-error'>{error}</p>
-          
+          <p className="form-error">{error}</p>
+
           <button type="submit">Sign in</button>
-          
-           <div className="or-section">
+
+          <div className="or-section">
             <span>or</span>
           </div>
-          
+
           <button type="button" className="google-signin">
             <img src={google} alt="Google Icon" />
             Sign in with Google
-          </button>  
+          </button>
 
-          
           <div className="create-account">
-            <a href="#" onClick={()=>setIslogin(false)}>New User? Create Account</a>
+            <a href="#" onClick={() => setIslogin(false)}>
+              New User? Create Account
+            </a>
           </div>
         </form>
-      </div>
+      ) : (
+        <form>
+          <h2>
+            <center>Reset Your Password</center>
+          </h2>
+          <label htmlFor="username">Enter Your Registered Email</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+
+          <p className="form-error">{error}</p>
+
+          <button onClick={requestresetpassword} className="submit-button">
+            Reset Password
+          </button>
+        </form>
+      )}
+    </div>
   );
 };
 
