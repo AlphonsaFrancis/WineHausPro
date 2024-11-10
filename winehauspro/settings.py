81d7978750afb11a5c5bd1c6b798386cba3f2d64
AlphonsaFrancis <155885284+AlphonsaFrancis@users.noreply.github.com>
2024-11-10 19:22:10 +0530

    updated

--- winehauspro/settings.py
+++ winehauspro/settings.py
@@ -147,12 +147,7 @@ WSGI_APPLICATION = 'winehauspro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+
 
 # DATABASES = {
 #     'default': {
@@ -177,8 +172,15 @@ WSGI_APPLICATION = 'winehauspro.wsgi.application'
 # }
 
 DATABASES = {
-    "default": dj_database_url.parse(config("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     "default": dj_database_url.parse(config("DATABASE_URL"))
+# }
 
 
 
