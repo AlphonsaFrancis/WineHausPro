6b919037e1b4fa5c2de2cb1a3c8fbca6975d310a
mrpulikkan <alphonsafr@gmail.com>
2025-01-19 20:53:25 +0530

    new user group- delivery agents

--- authentication/models.py
+++ authentication/models.py
@@ -1,57 +1,4 @@
-# from django.db import models
-# from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
 
-
-
-# class CustomUserManager(BaseUserManager):
-#     def create_user(self, email, password=None, **extra_fields):
-#         if not email:
-#             raise ValueError('The Email field must be set')
-#         email = self.normalize_email(email)
-#         user = self.model(email=email, **extra_fields)
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
- 
-#     def create_superuser(self, email, password=None, **extra_fields):
-#         extra_fields.setdefault('is_staff', True)
-#         extra_fields.setdefault('is_superuser', True)
- 
-#         return self.create_user(email, password, **extra_fields)
-    
-# class User(AbstractBaseUser,PermissionsMixin):
-#     # username=models.CharField(max_length=255,unique=True)
-#     email=models.CharField(max_length=255,unique=True)
-#     password=models.CharField(max_length=255)
-#     date_joined=models.DateTimeField(auto_now_add=True)
-#     is_staff=models.BooleanField(default=False)
-#     is_active=models.BooleanField(default=True)
-#     is_profile_completed=models.BooleanField(default=False) 
-#     is_superuser = models.BooleanField(default=False)
-#     last_login=models.DateTimeField(null=True)
-#     updated_at=models.DateTimeField(auto_now=True)
-#     created_at=models.DateTimeField(auto_now_add=True)
-
-#     USERNAME_FIELD='email'
-#     objects = CustomUserManager()
-#     def __str__(self):
-#         return self.email
-    
-    
-# class User_profile(models.Model):
-#     user=models.ForeignKey(User,on_delete=models.CASCADE)
-#     first_name=models.CharField(max_length=255)
-#     last_name=models.CharField(max_length=255)
-#     phone=models.CharField(max_length=255)
-#     default_address=models.CharField(max_length=255)
-#     default_city=models.CharField(max_length=255)
-#     default_state=models.CharField(max_length=255)
-#     default_pincode=models.CharField(max_length=255)
-#     updated_at=models.DateTimeField(auto_now=True)
-#     created_at=models.DateTimeField(auto_now_add=True)
-
-#     def __str__(self):
-#         return self.first_name
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
 
@@ -84,6 +31,7 @@ class User(AbstractBaseUser,PermissionsMixin):
     is_profile_completed=models.BooleanField(default=False) 
     is_superuser = models.BooleanField(default=False)
     last_login=models.DateTimeField(null=True)
+    is_delivery_agent = models.BooleanField(default=False)
     updated_at=models.DateTimeField(auto_now=True)
     created_at=models.DateTimeField(auto_now_add=True)
 
