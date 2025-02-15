da6e4596e7f90cd9fe3dddc2cbfa11c85ec3ffbc
mrpulikkan <155885284+AlphonsaFrancis@users.noreply.github.com>
2025-02-15 23:38:10 +0530

    Update settings.py

--- winehauspro/settings.py
+++ winehauspro/settings.py
@@ -96,7 +96,8 @@ AUTH_USER_MODEL = 'authentication.User'
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://projects.winehaus-be.alphonsafrancis.com'
 ]
 
 CORS_ALLOW_METHODS = [
