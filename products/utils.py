4009827d727165eeeeb19e61f64a4b245dd859a6
AlphonsaFrancis <155885284+AlphonsaFrancis@users.noreply.github.com>
2025-02-18 16:35:04 +0530

    update

--- products/utils.py
+++ products/utils.py
@@ -1,6 +1,8 @@
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 User = get_user_model()
 
@@ -9,29 +11,26 @@ def send_low_stock_notification(product):
     Send email notification to suppliers when stock is low
     """
     suppliers = User.objects.filter(is_supplier=True)
-    subject = f'Low Stock Alert: {product.name}'
-    message = f'''
-    Dear Supplier,
-
-    This is to notify you that the following product is running low on stock:
-
-    Product: {product.name}
-    Current Stock: {product.stock_quantity}
-    Minimum Threshold: {product.min_stock_threshold}
     
-    Please take necessary action to replenish the stock.
-
-    Best regards,
-    WineHaus Team
-    '''
+    # Prepare the email content using the template
+    html_message = render_to_string('email/low_stock_notification.html', {
+        'product': product,
+        'login_url': 'http://localhost:3000/login'  # Add your frontend login URL
+    })
+    
+    # Strip HTML tags for plain text version
+    plain_message = strip_tags(html_message)
+    
+    subject = f'Low Stock Alert: {product.name}'
     
     recipient_list = [supplier.email for supplier in suppliers]
     
     if recipient_list:
         send_mail(
             subject=subject,
-            message=message,
+            message=plain_message,
             from_email=settings.DEFAULT_FROM_EMAIL,
             recipient_list=recipient_list,
             fail_silently=False,
+            html_message=html_message
         ) 
