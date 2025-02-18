0119d2e857f3a11fe7a001867e3bff649b7f74c0
AlphonsaFrancis <155885284+AlphonsaFrancis@users.noreply.github.com>
2025-02-18 12:12:17 +0530

    commit

---
+++ src/utils/config.js
@@ -0,0 +1,5 @@
+// For development
+export const BASE_URL = 'http://localhost:8000';
+
+// For production (uncomment when deploying)
+// export const BASE_URL = 'https://your-production-api-url.com'; 
